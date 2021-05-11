da3fa47f6037e4d7e33dc983f3d4289bf7337a3b
KirillNN <wmforum@bk.ru>
2021-05-11 16:30:29 +0300

    Update codewars_7kyu.py

--- codewars_7kyu.py
+++ codewars_7kyu.py
@@ -174,4 +174,22 @@ def cool_string(s):
     # x = re.match('[a-z]{2}', s)
     return re.search('[^a-zA-Z]|[a-z]{2}|[A-Z]{2}', s) is None
 
-print(cool_string("lBC"))
+
+# print(cool_string("lBC"))
+
+
+# Given 2 strings, a and b, return a string of the form: shorter+reverse(longer)+shorter.
+# In other words, the shortest string has to be put as prefix and as suffix of the reverse of the longest.
+# Strings a and b may be empty, but not null (In C# strings may also be null. Treat them as if they are empty.).
+# If a and b have the same length treat a as the longer producing b+reverse(a)+b
+
+def shorter_reverse_longer(a, b):
+    return b + a[::-1] + b if len(a) >= len(b) else a + b[::-1] + a
+
+
+# print(shorter_reverse_longer("first", "abcde"))
+# print(shorter_reverse_longer("hello", "bau"))
+# print(shorter_reverse_longer("abcde", "fghi"))
+
+def not_visible_cubes(n):
+    return max(0, (n - 2) ** 3)
