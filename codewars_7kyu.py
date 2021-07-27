1c4ca841b6e1c9b8853cb1c49de8405fb3ef2eee
KirillNN <wmforum@bk.ru>
2021-07-28 00:19:25 +0300

    Sum

--- codewars_7kyu.py
+++ codewars_7kyu.py
@@ -327,10 +327,31 @@ def DNA_strand(dna):
 
 assert DNA_strand("ATTGC") == "TAACG"
 
+
 # https://www.codewars.com/kata/60dda5a66c4cf90026256b75/train/python
 # функция в функции
 def some_but_not_all(seq, pred):
     return any(map(pred, seq)) and not all(map(pred, seq))
 
 
-print(some_but_not_all('abcdefg&%$', str.isalpha))
+# print(some_but_not_all('abcdefg&%$', str.isalpha))
+assert some_but_not_all('abcdefg&%$', str.isalpha) == True
+
+
+# https://www.codewars.com/kata/5981a139f5471fd1b2000071/train/python
+def task(w, n, c):
+    names = ['James', 'John', 'Robert', 'Michael', 'William']
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    return f'It is {w} today, {names[days.index(w)]}, you have to work, you must spray {n} trees and you need {n * c} dollars to buy liquid'
+
+
+assert task('Wednesday', 10,
+            2) == 'It is Wednesday today, Robert, you have to work, you must spray 10 trees and you need 20 dollars to buy liquid'
+assert task('Monday', 4,
+            3) == 'It is Monday today, James, you have to work, you must spray 4 trees and you need 12 dollars to buy liquid'
+assert task('Friday', 5,
+            4) == 'It is Friday today, William, you have to work, you must spray 5 trees and you need 20 dollars to buy liquid'
+assert task('Tuesday', 6,
+            1) == 'It is Tuesday today, John, you have to work, you must spray 6 trees and you need 6 dollars to buy liquid'
+assert task('Thursday', 5,
+            3) == 'It is Thursday today, Michael, you have to work, you must spray 5 trees and you need 15 dollars to buy liquid'
