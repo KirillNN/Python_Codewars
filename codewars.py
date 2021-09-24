f0415d6c164ad84a1a6a5406156c786a24575e49
KirillNN <wmforum@bk.ru>
2021-09-24 17:07:23 +0300

    Update codewars.py

--- codewars.py
+++ codewars.py
@@ -2073,3 +2073,11 @@ def kata_13_december(lst):
 
 assert kata_13_december([1, 2, 2, 2, 4, 3, 4, 5, 6, 7]) == [1, 3, 5, 7]
 assert kata_13_december([1, 2, 2, 2, 4, 3, 4]) == [1, 3]
+
+
+# https://www.codewars.com/kata/56bcaedfcf6b7f2125001118/python
+# Замена символа в строке
+def html_special_chars(data):
+    symbols = {'<': '&lt;', '>': '&gt;', '"': '&quot;', '&': '&amp;'}
+    return "".join(symbols.get(x, x) for x in data)
+
