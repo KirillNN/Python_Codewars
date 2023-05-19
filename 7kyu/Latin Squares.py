56cb6bb248dd9a6715b7ff4e16aa09c78d24db8a
KirillNN <wmforum@bk.ru>
2023-05-20 00:16:54 +0300

    Add Latin Squares

---
+++ 7kyu/Latin Squares.py
@@ -0,0 +1,7 @@
+def make_latin_square(n):
+    ## Make all rows 1,2,...,n
+    latin_square = [[i for i in range(1, n + 1)] for _ in range(1, n + 1)]
+
+    ## Make your changes!
+
+    return latin_square
