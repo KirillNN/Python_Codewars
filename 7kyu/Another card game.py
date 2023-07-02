bb96cb9c0399378288342a976f575cdaa9746250
KirillNN <wmforum@bk.ru>
2023-07-02 17:23:32 +0300

    adds

--- 7kyu/Another card game.py
+++ 7kyu/Another card game.py
@@ -1,8 +1,2 @@
 def solution(frank, sam, tom):
-    if 0 in frank:
-        return 1
-    return 2
-
-
-print(solution([0, 4, 10, 11], [3, 6, 8, 9], [1, 2, 5, 7]))
-print(solution([1, 2, 4, 5], [3, 6, 8, 10], [0, 7, 9, 11]))
+    return frank[2] > sam[0] and frank[2] > tom[0] and frank[3] > sam[1] and frank[3] > tom[1]
